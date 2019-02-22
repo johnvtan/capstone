b45c5279b19819e249657b768bbd1922b27ee69e
John Tan <john.v.tan@gmail.com>
2019-02-22 15:53:35 -0500

    refactor

--- src/pcl_test/include/pcl_test/plane_segmentation.h
+++ src/pcl_test/include/pcl_test/plane_segmentation.h
@@ -11,11 +11,14 @@
 #include <pcl/ModelCoefficients.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/sac_model_plane.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/features/normal_3d.h>
+#include <pcl/common/geometry.h>
 
 #include <string>
+#include <deque>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
@@ -27,11 +30,25 @@ public:
     void run(void);
 private:
     void pointcloud_callback(const PointCloud::ConstPtr& msg);
-    bool new_pointcloud;
+    pcl::PointXYZ compute_centroid(const PointCloud::Ptr& pointcloud);
+    double get_rectangular_area(const PointCloud::Ptr& pointcloud);
+    bool near_previous_centroid(const pcl::PointXYZ& centroid_point);
+    PointCloud::Ptr filter_by_std_dev(const PointCloud::Ptr& pointcloud,
+                                      const pcl::ModelCoefficients::Ptr coefficients,
+                                      unsigned int num_dev);
+    double get_deviation(const PointCloud::Ptr& pointcloud,
+                         const pcl::ModelCoefficients::Ptr& coefficients,
+                         double* mean_error);
 
+    PointCloud::Ptr extract_cloud(const PointCloud::Ptr& pointcloud, const pcl::PointIndices::Ptr& indices, bool remove);
+
+    uint32_t colors[100];
+    std::deque< std::vector<pcl::PointXYZ>> centroid_buffer;
+    int frame_count;
     ros::NodeHandle nh;
     ros::Subscriber pointcloud_sub;
     ros::Publisher plane_pointcloud_pub;
+    ros::Publisher colored_plane_pub;
 
     pcl::VoxelGrid<pcl::PCLPointCloud2> voxel_filter;
     pcl::SACSegmentation<pcl::PointXYZ> plane_segmenter;
--- src/pcl_test/src/plane_segmentation.cpp
+++ src/pcl_test/src/plane_segmentation.cpp
@@ -56,14 +56,12 @@ void PlaneSegmentation::pointcloud_callback(const PointCloud::ConstPtr& msg)
     // convert to PointCloud
     pcl::fromPCLPointCloud2(*filtered_cloud2, *cloud_filtered);
 
-    // do plane segmentation lol
+    // do plane segmentation
     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices); 
     
     int num_points = (int)cloud_filtered->points.size();
     PointCloud::Ptr removed(new PointCloud);
-    PointCloud::Ptr plane(new PointCloud);
-    pcl::PointCloud<pcl::Normal>::Ptr plane_normals(new pcl::PointCloud<pcl::Normal>());
     PointCloud::Ptr all_planar(new PointCloud);
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr all_planar_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
 
@@ -76,12 +74,34 @@ void PlaneSegmentation::pointcloud_callback(const PointCloud::ConstPtr& msg)
             ROS_WARN("Could not find plane in this model");
             break;
         }
-        extracter.setInputCloud(cloud_filtered);
-        extracter.setIndices(inliers);
-        extracter.setNegative(false);
-        extracter.filter(*plane);
 
-        pcl::CentroidPoint<pcl::PointXYZ> curr_centroid;
+        PointCloud::Ptr plane_raw = extract_cloud(cloud_filtered, inliers, false);
+
+        // extract points within standard deviation
+        PointCloud::Ptr plane = filter_by_std_dev(plane_raw, coefficients, 2);
+       
+        // Find rectangular area and density of pointcloud
+        double rect_area = get_rectangular_area(plane);
+        double density = double(plane->points.size()) / rect_area;
+
+        // Try to find surface area of the pointcloud
+        
+        // dump info to console
+        std::cout << "-----------------NEW PLANE: " << plane_count << " ---------------" << std::endl;
+        std::cout << "Plane size: " <<  plane->points.size() << std::endl;
+        std::cout << "Area is: " << rect_area << std::endl;
+        std::cout << "Density is: " << density << std::endl;
+         
+        // Filter by density and comparison to previous centroids
+        pcl::PointXYZ centroid_point = compute_centroid(plane);
+        if (density > 70 && near_previous_centroid(centroid_point) || centroid_buffer.size() == 0) {
+            std::cout << "**************PUBLISHED**************" << std::endl;
+            plane_count++;
+            plane_centroids.push_back(centroid_point);
+            *all_planar += *plane;
+        }
+
+        // create colored pointcloud
         for (int i = 0; i < plane->points.size(); i++) {
             pcl::PointXYZRGB colored_point;
             pcl::PointXYZ point = plane->points.at(i);
@@ -91,63 +111,10 @@ void PlaneSegmentation::pointcloud_callback(const PointCloud::ConstPtr& msg)
 
             colored_point.rgb = colors[plane_count]; 
             all_planar_colored->points.push_back(colored_point);
-            curr_centroid.add(point);
         }
 
-        auto max_x_iter = std::max_element(plane->points.begin(), plane->points.end(), 
-                                           [](pcl::PointXYZ a, pcl::PointXYZ b) -> bool
-                                           {
-                                                return a.x < b.x;
-                                           });
-        auto  max_y_iter = std::max_element(plane->points.begin(), plane->points.end(), 
-                                            [](pcl::PointXYZ a, pcl::PointXYZ b) -> bool
-                                            {
-                                                 return a.y < b.y;
-                                            });
-        auto min_x_iter = std::min_element(plane->points.begin(), plane->points.end(), 
-                                           [](pcl::PointXYZ a, pcl::PointXYZ b) -> bool
-                                           {
-                                                return a.x < b.x;
-                                           });
-        auto min_y_iter = std::min_element(plane->points.begin(), plane->points.end(), 
-                                           [](pcl::PointXYZ a, pcl::PointXYZ b) -> bool
-                                           {
-                                                return a.y < b.y;
-                                           });
-        
-        pcl::PointXYZ centroid_point;
-        curr_centroid.get(centroid_point);
-        bool result = false;
-        for (int i = 0; i < centroid_buffer.size(); i++) {
-            for (int j = 0; j < centroid_buffer[i].size(); j++) {
-                pcl::PointXYZ prev_point = centroid_buffer[i][j];
-                double squ_dist = std::pow((centroid_point.x - prev_point.x), 2) +
-                                  std::pow((centroid_point.y - prev_point.y), 2) + 
-                                  std::pow((centroid_point.z - prev_point.z), 2);
-
-                if (std::sqrt(squ_dist) < 1.0) {
-                    result = true;
-                    break;
-                }
-            }
-        }
-           
-
-        ROS_INFO("Plane size: %lu; Deviation = %lf", plane->points.size(), get_deviation(plane, coefficients));
-        double area = ((*max_x_iter).x - (*min_x_iter).x) * ((*max_y_iter).y - (*min_y_iter).y);
-        std::cout << "Area is: " << area << std::endl;
-        
-        double density = double(plane->points.size()) / area;
-        std::cout << "Density is: " << density << std::endl;
-
-        if (density > 70 && result || centroid_buffer.size() == 0) {
-            plane_count++;
-            plane_centroids.push_back(centroid_point);
-            *all_planar += *plane;
-        }
-
-        extracter.setNegative(true);
-        extracter.filter(*removed);
+        // remove this plane from the rest of the pointcloud
+        removed = extract_cloud(cloud_filtered, inliers, true);
         cloud_filtered.swap(removed);
         plane_count++;
     }
@@ -156,6 +123,7 @@ void PlaneSegmentation::pointcloud_callback(const PointCloud::ConstPtr& msg)
     if (centroid_buffer.size() > 3) {
         centroid_buffer.pop_front();
     }
+
     std::cout << "-----------------------------------Plane count is: " << plane_count << std::endl;
     all_planar->header.frame_id = "guidance";
     pcl_conversions::toPCL(ros::Time::now(), all_planar->header.stamp);
@@ -166,11 +134,97 @@ void PlaneSegmentation::pointcloud_callback(const PointCloud::ConstPtr& msg)
     colored_plane_pub.publish(all_planar_colored);
 }
 
+pcl::PointXYZ PlaneSegmentation::compute_centroid(const PointCloud::Ptr& pointcloud) {
+    // calculate centroid of the plane
+    pcl::CentroidPoint<pcl::PointXYZ> curr_centroid;
+    for (int i = 0; i < pointcloud->points.size(); i++) {
+        pcl::PointXYZ point = pointcloud->points.at(i);
+        curr_centroid.add(point);
+    }
+    pcl::PointXYZ centroid_point;
+    curr_centroid.get(centroid_point);
+    return centroid_point;
+}
+
+double PlaneSegmentation::get_rectangular_area(const PointCloud::Ptr& pointcloud) {
+    // Find (min_x, min_y), (max_x, max_y) to get a rectangular area of the pointcloud
+    auto max_x_iter = std::max_element(pointcloud->points.begin(), pointcloud->points.end(), 
+                                       [](pcl::PointXYZ a, pcl::PointXYZ b) -> bool
+                                       {
+                                            return a.x < b.x;
+                                       });
+    auto  max_y_iter = std::max_element(pointcloud->points.begin(), pointcloud->points.end(), 
+                                        [](pcl::PointXYZ a, pcl::PointXYZ b) -> bool
+                                        {
+                                             return a.y < b.y;
+                                        });
+    auto min_x_iter = std::min_element(pointcloud->points.begin(), pointcloud->points.end(), 
+                                       [](pcl::PointXYZ a, pcl::PointXYZ b) -> bool
+                                       {
+                                            return a.x < b.x;
+                                       });
+    auto min_y_iter = std::min_element(pointcloud->points.begin(), pointcloud->points.end(), 
+                                       [](pcl::PointXYZ a, pcl::PointXYZ b) -> bool
+                                       {
+                                            return a.y < b.y;
+                                       });
+ 
+    return ((*max_x_iter).x - (*min_x_iter).x) * ((*max_y_iter).y - (*min_y_iter).y);
+}
+
+bool PlaneSegmentation::near_previous_centroid(const pcl::PointXYZ& centroid_point) {
+    // compare the centroid of the current cloud to centroids in the buffer. 
+    // Only consider this plane if it's centroid was close (<1m) from a centroid seen within the last 3 frames 
+    bool result = false;
+    for (int i = 0; i < centroid_buffer.size(); i++) {
+        for (int j = 0; j < centroid_buffer[i].size(); j++) {
+            pcl::PointXYZ prev_point = centroid_buffer[i][j];
+            double squ_dist = std::pow((centroid_point.x - prev_point.x), 2) +
+                              std::pow((centroid_point.y - prev_point.y), 2) + 
+                              std::pow((centroid_point.z - prev_point.z), 2);
+
+            if (std::sqrt(squ_dist) < 1.0) {
+                result = true;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+PointCloud::Ptr PlaneSegmentation::filter_by_std_dev(const PointCloud::Ptr& pointcloud,
+                                                     const pcl::ModelCoefficients::Ptr coefficients,
+                                                     unsigned int num_dev)
+{
+    // filter out any points more than two standard deviations from the cloud
+    pcl::PointIndices::Ptr points_within_n_std_dev(new pcl::PointIndices);
+    double mean_distance = 0;
+    double dev = get_deviation(pointcloud, coefficients, &mean_distance);
+    for (int i = 0; i < pointcloud->points.size(); i++) {
+        pcl::PointXYZ point = pointcloud->points.at(i);
+
+        // compute distance
+        double c1 = coefficients->values[0];
+        double c2 = coefficients->values[1];
+        double c3 = coefficients->values[2];
+        double c4 = coefficients->values[3];
+
+        double d = pcl::pointToPlaneDistance<pcl::PointXYZ>(point, c1, c2, c3, c4); // in meters
+        if (std::abs(d - mean_distance) < num_dev * dev) {
+            // then we keep this point since we're within two std devs
+            points_within_n_std_dev->indices.push_back(i); 
+        }
+    }
+
+    return extract_cloud(pointcloud, points_within_n_std_dev, false);
+}
+
 double PlaneSegmentation::get_deviation(const PointCloud::Ptr& pointcloud,
-                                        const pcl::ModelCoefficients::Ptr& coefficients) {
+                                        const pcl::ModelCoefficients::Ptr& coefficients,
+                                        double* mean_error) {
 
     std::vector<double> errors;
-    double mean_error = 0;
+    *mean_error = 0;
     double min_error = 100000;
     double max_error = 0;
     for (int i = 0; i < pointcloud->points.size(); i++) {
@@ -189,7 +243,7 @@ double PlaneSegmentation::get_deviation(const PointCloud::Ptr& pointcloud,
 
         // update statistics
         errors.push_back(d);
-        mean_error += d;
+        *mean_error += d;
         if (d > max_error) {
             max_error = d;
         }
@@ -197,12 +251,26 @@ double PlaneSegmentation::get_deviation(const PointCloud::Ptr& pointcloud,
             min_error = d;
         }
     }
-    mean_error /= pointcloud->points.size();
+    *mean_error /= pointcloud->points.size();
 
     double deviation = 0;
     for (int i = 0; i < pointcloud->points.size(); i++) {
-        deviation += std::pow(errors[i] - mean_error, 2);
+        deviation += std::pow(errors[i] - *mean_error, 2);
     }
 
     return std::sqrt(deviation / pointcloud->points.size());
+}
+
+PointCloud::Ptr PlaneSegmentation::extract_cloud(const PointCloud::Ptr& pointcloud,
+                                                 const pcl::PointIndices::Ptr& indices,
+                                                 bool remove)
+{
+    PointCloud::Ptr return_cloud(new PointCloud);
+    pcl::ExtractIndices<pcl::PointXYZ> extracter;
+
+    extracter.setInputCloud(pointcloud);
+    extracter.setIndices(indices);
+    extracter.setNegative(remove);
+    extracter.filter(*return_cloud);
+    return return_cloud;
 }
